3034ced8e2a887157a6dfd6df968df8a1e18be50
ericmasila <ericmasila254@gmail.com>
2023-09-19 00:50:08 -0700

    handle flags

--- main.h
+++ main.h
@@ -2,7 +2,15 @@
 #define MAIN_H
 
 #define END '\0'
-
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
