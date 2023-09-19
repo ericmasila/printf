e51a138476584f3d793a3d2a6f3c169f184d9b03
ericmasila <ericmasila254@gmail.com>
2023-09-19 00:57:05 -0700

    handle flag

--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 #define END '\0'
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 /* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
@@ -11,6 +14,7 @@
 /* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
